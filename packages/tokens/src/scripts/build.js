2305b95207a5b8c41925ccd42ec0624c9c2f6f9a
Lucas Viana <contato@lucasviana.dev>
2023-05-30 12:53:24 -0300

    :hammer: updates build script to rename tokens

--- packages/tokens/src/scripts/build.js
+++ packages/tokens/src/scripts/build.js
@@ -7,60 +7,57 @@ const formats = {
   colors: {
     name: 'javascript/colors',
     formatter(dictionary) {
-      return `
-  export const colors = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${prop.value}',`)
-    .join('\n')}
-  };`;
+      return `export const colors = {
+${dictionary.allTokens
+  .map(renamePropName)
+  .join('\n')}
+};`;
     },
   },
   shadows: {
     name: 'javascript/shadows',
     formatter(dictionary) {
-      return `
-  export const shadows = {
-  ${dictionary.allTokens
-    .map(
-      (prop) =>
-        `  ${prop.name}: '${prop.value.x} ${prop.value.y} ${prop.value.blur} ${prop.value.spread} ${prop.value.color}',`
-    )
-    .join('\n')}
-  };
-  `;
+      return `export const shadows = {
+${dictionary.allTokens
+  .map(
+    (prop) => {
+      const [, ...words] = splitCamelCase(prop.name);
+      const name = toCamelCase(words)
+      return `  ${name}: '${prop.value.x} ${prop.value.y} ${prop.value.blur} ${prop.value.spread} ${prop.value.color}',`  
+    })
+  .join('\n')}
+};
+`;
     },
   },
   fonts: {
     name: 'javascript/fonts',
     formatter(dictionary) {
-      return `
-  export const fonts = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${prop.value}',`)
-    .join('\n')}
-  };
-  `;
+      return `export const fonts = {
+${dictionary.allTokens
+  .map(renamePropName)
+  .join('\n')}
+};
+`;
     },
   },
   radius: {
     name: 'javascript/radius',
     formatter(dictionary) {
-      return `
-  export const radii = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${prop.value}',`)
-    .join('\n')}
-  };
-  `;
+      return `export const radii = {
+${dictionary.allTokens
+  .map(renamePropName)
+  .join('\n')}
+};
+`;
     },
   },
   borderWidths: {
     name: 'javascript/border-widths',
     formatter(dictionary) {
-      return `
-export const borderWidths = {
+      return `export const borderWidths = {
 ${dictionary.allTokens
-  .map((prop) => `  ${prop.name}: '${prop.value}',`)
+  .map(renamePropName)
   .join('\n')}
 };
   `;
@@ -78,34 +75,35 @@ ${dictionary.allTokens
         return `${remValue}rem`;
       };
 
-      return `
-  export const space = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${pxToRem(prop.value)}',`)
-    .join('\n')}
-  };
-  `;
+      return `export const space = {
+${dictionary.allTokens
+  .map((prop) => {
+    const [, ...words] = splitCamelCase(prop.name);
+    const name = toCamelCase(words)
+    return `  ${name}: '${pxToRem(prop.value)}',`
+  })
+  .join('\n')}
+};
+`;
     },
   },
   lineHeights: {
     name: 'javascript/line-heights',
     formatter(dictionary) {
-      return `
-  export const lineHeights = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${prop.value}',`)
-    .join('\n')}
-  };
-  `;
+      return `export const lineHeights = {
+${dictionary.allTokens
+  .map(renamePropName)
+  .join('\n')}
+};
+`;
     },
   },
   fontSizes: {
     name: 'javascript/font-sizes',
     formatter(dictionary) {
-      return `
-export const fontSizes = {
+      return `export const fontSizes = {
 ${dictionary.allTokens
-  .map((prop) => `  ${prop.name}: '${prop.value}',`)
+  .map(renamePropName)
   .join('\n')}
 };
   `;
@@ -114,15 +112,9 @@ ${dictionary.allTokens
   fontWeights: {
     name: 'javascript/font-weights',
     formatter(dictionary) {
-      return `
-export const fontWeights = {
+      return `export const fontWeights = {
 ${dictionary.allTokens
-  .map(
-    (prop) =>
-      `  ${prop.value === 'regular' ? `${prop.path[0]}Normal` : prop.name}: '${
-        prop.value === 'regular' ? 'normal' : prop.value
-      }',`
-  )
+  .map(renamePropName)
   .join('\n')}
 };
   `;
@@ -131,13 +123,12 @@ ${dictionary.allTokens
   opacity: {
     name: 'javascript/opacity',
     formatter(dictionary) {
-      return `
-  export const opacity = {
-  ${dictionary.allTokens
-    .map((prop) => `  ${prop.name}: '${prop.value}',`)
-    .join('\n')}
-  };
-  `;
+      return `export const opacity = {
+${dictionary.allTokens
+  .map(renamePropName)
+  .join('\n')}
+};
+`;
     },
   },
 };
@@ -244,77 +235,73 @@ Object.entries(filters).forEach(([, filter]) =>
   StyleDictionaryPackage.registerFilter(filter)
 );
 
-function getStyleDictionaryConfig() {
-  return {
-    source: [`src/tokens.globals.json`],
-    platforms: {
-      'web/js': {
-        transformGroup: 'tokens-js',
-        files: [
-          {
-            destination: 'src/constants/colors.ts',
-            format: 'javascript/colors',
-            filter: 'isColor',
-          },
-          {
-            destination: 'src/constants/shadows.ts',
-            format: 'javascript/shadows',
-            filter: 'isShadow',
-          },
-          {
-            destination: 'src/constants/fonts.ts',
-            format: 'javascript/fonts',
-            filter: 'isFont',
-          },
-          {
-            destination: 'src/constants/radius.ts',
-            format: 'javascript/radius',
-            filter: 'isRadius',
-          },
-          {
-            destination: 'src/constants/borderWidths.ts',
-            format: 'javascript/border-widths',
-            filter: 'isBorderWidth',
-          },
-          {
-            destination: 'src/constants/space.ts',
-            format: 'javascript/spaces',
-            filter: 'isSpace',
-          },
-          {
-            destination: 'src/constants/lineHeights.ts',
-            format: 'javascript/line-heights',
-            filter: 'isLineHeight',
-          },
-          {
-            destination: 'src/constants/fontSizes.ts',
-            format: 'javascript/font-sizes',
-            filter: 'isFontSize',
-          },
-          {
-            destination: 'src/constants/fontWeights.ts',
-            format: 'javascript/font-weights',
-            filter: 'isFontWeight',
-          },
-          {
-            destination: 'src/constants/opacity.ts',
-            format: 'javascript/opacity',
-            filter: 'isOpacity',
-          },
-        ],
-      },
+const styleDictionaryConfig = {
+  source: [`src/tokens.globals.json`],
+  platforms: {
+    'web/js': {
+      transformGroup: 'tokens-js',
+      files: [
+        {
+          destination: 'src/constants/colors.ts',
+          format: 'javascript/colors',
+          filter: 'isColor',
+        },
+        {
+          destination: 'src/constants/shadows.ts',
+          format: 'javascript/shadows',
+          filter: 'isShadow',
+        },
+        {
+          destination: 'src/constants/fonts.ts',
+          format: 'javascript/fonts',
+          filter: 'isFont',
+        },
+        {
+          destination: 'src/constants/radius.ts',
+          format: 'javascript/radius',
+          filter: 'isRadius',
+        },
+        {
+          destination: 'src/constants/borderWidths.ts',
+          format: 'javascript/border-widths',
+          filter: 'isBorderWidth',
+        },
+        {
+          destination: 'src/constants/space.ts',
+          format: 'javascript/spaces',
+          filter: 'isSpace',
+        },
+        {
+          destination: 'src/constants/lineHeights.ts',
+          format: 'javascript/line-heights',
+          filter: 'isLineHeight',
+        },
+        {
+          destination: 'src/constants/fontSizes.ts',
+          format: 'javascript/font-sizes',
+          filter: 'isFontSize',
+        },
+        {
+          destination: 'src/constants/fontWeights.ts',
+          format: 'javascript/font-weights',
+          filter: 'isFontWeight',
+        },
+        {
+          destination: 'src/constants/opacity.ts',
+          format: 'javascript/opacity',
+          filter: 'isOpacity',
+        },
+      ],
     },
-  };
-}
+  },
+};
 
 console.log('Build started...');
 
 console.log('\n==============================================');
-console.log(`\nProcessing: Globals`);
+console.log(`\nProcessing: Tokens`);
 
-const StyleDictionary = StyleDictionaryPackage.extend(
-  getStyleDictionaryConfig()
-);
+const StyleDictionary = StyleDictionaryPackage.extend(styleDictionaryConfig);
 
 StyleDictionary.buildPlatform('web/js');
 
@@ -322,3 +309,24 @@ console.log('\nEnd processing');
 
 console.log('\n==============================================');
 console.log('\nBuild completed!');
+
+function splitCamelCase(s) {
+  return s.split(/(?=[A-Z])/);
+}
+
+function toCamelCase(words) {
+  if (words.length === 0) return '';
+
+  return words.map((word, index) => {
+      if (index === 0) {
+          return word.toLowerCase();
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join('');
+}
+
+function renamePropName(prop) {
+  const [, ...words] = splitCamelCase(prop.name);
+  const name = toCamelCase(words)
+  return `  ${name}: '${prop.value}',`
+}
